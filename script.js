a63e6aca926322daa050d19fa81f99c171987f4a
Pramodkar21 <karthi21@msu.edu>
2026-03-25 09:17:27 +0000

    Add initial HTML and fetch logic for JSONPlaceholder data

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    const output = document.getElementById('output');
+
+    output.innerHTML = data
+      .map(item => {
+        return `
+          <div style="margin-bottom: 20px;">
+            <h2>${item.title}</h2>
+            <p>${item.body}</p>
+          </div>
+        `;
+      })
+      .join('');
+  });
 
